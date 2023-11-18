bf2c9614971bfc4cde0a9a8109e4846fbaee1b23
DESKTOP-9GRKLB4\Dao Uyen <uyendaothi@bizfly.vn>
2023-11-18 15:05:47 +0700

    use redux to add and delete user

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -7,7 +7,23 @@ const initState = {
     ]
 }
 const rootReducer = (state = initState, action) => {
-
+    switch (action.type) {
+        case 'DELETE_USER':
+            console.log('run delete', action);
+            let users = state.users
+            users = users.filter(item => item.id !== action.payload.id)
+            return {
+                ...state, users
+            };
+        case 'CREATE_USER':
+            let id = Math.floor(Math.random() * 10000)
+            let user = { id: id, name: `random - ${id}` }
+            return {
+                ...state, users: [...state.users, user]
+            }
+        default:
+            return state;
+    }
     return state;
 }
 
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -11,13 +11,34 @@ class Home extends React.Component {
         //     this.props.history.push('/todo')
         // }, 3000)
     }
+    handleDeleteUser = (user) => {
+        this.props.deleteUserRedux(user);
+    }
+
+    handleCreateUser = () => {
+        this.props.addUserRedux()
+    }
 
     render() {
         console.log('>>> check data Redux', this.props);
+        let listUser = this.props.dataRedux;
         return (
             <>
                 <div>Hello world</div>
                 <img src={logo} />
+                <div>
+                    {listUser && listUser.length > 0 &&
+                        listUser.map((item, index) => {
+                            return (
+                                <div>
+                                    {index + 1} - {item.name}
+                                    <span onClick={() => this.handleDeleteUser(item)}>x</span>
+                                </div>
+                            )
+                        })
+                    }
+                    <button type="button" onClick={() => this.handleCreateUser()}>Add</button>
+                </div>
             </>
 
         )
@@ -27,5 +48,12 @@ class Home extends React.Component {
 const mapStateToProps = (state) => {
     return { dataRedux: state.users }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (userDelete) => dispatch({ type: 'DELETE_USER', payload: userDelete }),
+        addUserRedux: () => dispatch({ type: 'CREATE_USER' })
+    }
+}
 //HOC: higher order component -> tạo ra 1 component mới với đầy đủ thuộc tính của thằng bên ngoài + của thằng bạn đang có
-export default connect(mapStateToProps)(Color(Home));
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
