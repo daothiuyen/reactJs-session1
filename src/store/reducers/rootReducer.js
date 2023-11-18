7df6b6f4086193c20f40f5fc01dbee3f0f04a635
DESKTOP-9GRKLB4\Dao Uyen <uyendaothi@bizfly.vn>
2023-11-18 14:31:38 +0700

    mapping state redux to props react

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,7 +2,8 @@
 const initState = {
     users: [
         { id: 1, name: 'Uyên' },
-        { id: 2, name: 'Bắc' }
+        { id: 2, name: 'Bắc' },
+        { id: 3, name: 'Hehe' },
     ]
 }
 const rootReducer = (state = initState, action) => {
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
 import logo from '../../assets/images/logo.jpg';
+import { connect } from 'react-redux';
 
 class Home extends React.Component {
 
@@ -12,6 +13,7 @@ class Home extends React.Component {
     }
 
     render() {
+        console.log('>>> check data Redux', this.props);
         return (
             <>
                 <div>Hello world</div>
@@ -21,5 +23,9 @@ class Home extends React.Component {
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return { dataRedux: state.users }
+}
 //HOC: higher order component -> tạo ra 1 component mới với đầy đủ thuộc tính của thằng bên ngoài + của thằng bạn đang có
-export default Color(Home);
+export default connect(mapStateToProps)(Color(Home));
